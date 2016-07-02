36f3b061efcc86e98d8457b7da9727a68a1277c3
Russell Barnhart <russell@tumblr.com>
2016-07-02 13:38:06 -0400

    add class locator

---
+++ tests/fs/locator/ClassLocatorTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace sndsgd\fs\locator;
+
+use \org\bovigo\vfs\vfsStream;
+
+/**
+ * @coversDefaultClass \sndsgd\fs\locator\ClassLocator
+ */
+class ClassLocatorTest extends \PHPUnit_Framework_TestCase
+{
+    private function createValidFilter()
+    {
+        return function (\ReflectionClass $class): bool
+        {
+            return !$class->isAbstract();
+        };
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testConstructor()
+    {
+        $locator = new ClassLocator();
+    }
+
+    /**
+     * @covers ::setFilter
+     * @dataProvider providerSetFilter
+     */
+    public function testSetFilter(bool $isFilterValid = false)
+    {
+        $validatorMock = $this->getMockBuilder(ClassLocatorFilterValidator::class)
+            ->setMethods(["validate"])
+            ->getMock();
+
+        if (!$isFilterValid) {
+            $this->setExpectedException(\InvalidArgumentException::class);
+            $validatorMock
+                ->method("validate")
+                ->will($this->throwException(new \Exception()));
+
+        } else {
+            $validatorMock
+                ->method("validate")
+                ->willReturn(true);
+        }
+
+        $locator = new ClassLocator(null, $validatorMock);
+        $locator->setFilter($this->createValidFilter());
+    }
+
+    public function providerSetFilter()
+    {
+        return [
+            [false],
+            [true],
+        ];
+    }
+
+    public function testEverything()
+    {
+        $locator = new ClassLocator($this->createValidFilter());
+        # search the repo root
+        $locator->searchDir(__DIR__."/../../..", false);
+        # search the src directory recursively
+        $locator->searchDir(__DIR__."/../../../src", true);
+        # re-search to test dkipping already searched directories
+        $locator->searchDir(__DIR__."/../../../src", true);
+
+        # test getting a list of classes found
+        foreach ($locator->getClasses() as $class) {
+            $this->assertTrue(class_exists($class));
+        }
+
+        # test getting a list of reflections for the classes found
+        foreach ($locator->getReflectionClasses() as $reflection) {
+            $this->assertInstanceOf(\ReflectionClass::class, $reflection);
+        }
+    }
+}
